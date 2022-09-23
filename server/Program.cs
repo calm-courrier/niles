d7e8bfaf20ecb6e1c05fbb559641bdad04a0baf5
Calm Courrier <calmcourrier@gmail.com>
2022-09-23 22:41:50 +0200

    Initial project generated

---
+++ server/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//Redirect to SPA if path does not start with /api and does not have extension (static file)
+app.Use(async (context, next) =>
+{
+    await next();
+    var path = context.Request.Path.Value;
+
+    if (path != null && !path.StartsWith("/api") && !Path.HasExtension(path)) {
+        context.Request.Path = "/index.html";
+        await next();
+    }
+});
+
+app.MapControllers();
+
+// Don't host SPA static files for DEV - use "npm run dev" on client instead
+if (!app.Environment.IsDevelopment()) {
+    app.UseStaticFiles(new StaticFileOptions
+        { FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "app")) });
+}
+
+app.Run();
